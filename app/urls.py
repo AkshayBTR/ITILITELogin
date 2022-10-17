a19204bea2047c929d70560faee4ee97b1f84880
akshay-vk64-admin <akshay.v@testyantra.com>
2022-10-17 17:22:14 +0530

    first commit

---
+++ app/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name="app"
+
+urlpatterns = [
+    path('',views.index,name="index"),
+    path('home/',views.home,name="home"),
+    path('hai/<name>',views.hainame,name="hai"),
+    path('add/<a>/<b>',views.add,name="add"),
+    path('temp/',views.tempdemo,name="tempdemo"),
+    path('grt/<a>/<b>',views.grt2,name="grt2"),
+    path('homepage/',views.hometemp,name="hometemp"),
+    path('aboutus/',views.abouttemp,name="aboutus"),
+    path('register/',views.register,name="register"),
+    path("login/",views.user_login,name="login"),
+    path("logout/",views.user_logout,name="logout"),    
+    # path("profile/<id>",views.show_data,name="profile"),
+]
